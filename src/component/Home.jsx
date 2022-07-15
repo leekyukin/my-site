d9c071dd6e5e37ccd75ec5a2951be117cc7644c9
leekyukin <leekujin14@gmail.com>
2022-07-15 23:48:37 +0900

    ADD :: Home Component

---
+++ src/component/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import jesus from '../img/can.jpg';
+
+const MyComponent = () => {
+  return (
+    <div className="home">
+      <img className="background-image" src={jesus}/>
+    </div>
+  );
+};
+
+export default MyComponent;
