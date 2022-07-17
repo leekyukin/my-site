d656aaeb223e9c20ef7f931bc70511ab5d9fde58
leekyukin <leekujin14@gmail.com>
2022-07-17 12:18:45 +0900

    ADD :: SNS component

--- src/component/pages/SNS.jsx
+++ src/component/pages/SNS.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
-import instagram from "../../img/insta.png"
+import instagram from "../../img/sns/instagram.png"
+import tistory from '../../img/sns/tistory.png';
+import kakao from '../../img/sns/kakao.png';
+import velog from '../../img/sns/velog.jpg';
+import github from '../../img/sns/githug.png';
+
 
 const MyComponent = () => {
+
   return (
-    <div className="SNS">
+    <div className="SNS main">
       <a href="https://www.instagram.com/x_xxi_lkj/">
-        <img src={instagram}/>
+          <img className="sns-icon" src={instagram} />
       </a>
-      <img src={""}/>
+      <a href="https://phillnam.tistory.com">
+        <img className="sns-icon" src={tistory}/>
+      </a>
+      <a href="https://github.com/leekyukin">
+        <img className="sns-icon" src={github}/>
+      </a>
+      <a href="https://velog.io/@leekyukin">
+        <img className="sns-icon" src={velog}/>
+      </a>
+      <a href="https://open.kakao.com/o/ssLIeuqe">
+        <img className="sns-icon" src={kakao}/>
+      </a>
     </div>
   );
 };
